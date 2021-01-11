30ae8f6a3817ba6c89d79a393f0b0553c9122cba
sumitkumar <sumitk.iitkgp@gmail.com>
2021-01-11 23:53:04 +0530

    Flask App Addition

--- application.py
+++ application.py
@@ -1,17 +1,15 @@
 from flask import Flask
 
-# print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
 
-# some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+    <html>\n<head> <title>NewVillage | 2021 Version</title> </head>\n<body>'''
 instructions = '''
     <p><em>Hint</em>: This is a RESTful web service! Append a username
     to the URL (for example: <code>/Thelonious</code>) to say hello to
     someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
+home_link = '<p><a href="/">Back Home</a></p>\n'
 footer_text = '</body>\n</html>'
 
 # EB looks for an 'application' callable by default.
@@ -21,14 +19,12 @@ application = Flask(__name__)
 application.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
+# add a rule when the page is accessed with a name appended to the site URL.
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
+    # Setting debug to True enables debug output. This line should be removed before deploying a production app.
     application.debug = True
     application.run()
